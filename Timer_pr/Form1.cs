f92a889d86623a7d3757f0a7e9fbd08c8b9d6809
zzex3on___yaho <s2017s34@gmail.com>
2019-05-27 09:04:10 +0900

    #2 타이머 기능 구현 및 알림 추가, Information 부분에서 오류발생..

--- Timer_pr/Form1.cs
+++ Timer_pr/Form1.cs
@@ -7,8 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-// using Microsoft VisualBasic;
+using Microsoft.VisualBasic;
 
 namespace Timer_pr
 {
@@ -24,6 +23,60 @@ namespace Timer_pr
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            if(intCheck())
+            {
+                this.CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.txtNum.ReadOnly = true;
+                this.Timer.Enabled = true;
+            }
+            else
+            {
+                this.txtNum.Focus();
+                this.txtNum.Text = "";
+            }
+        }
+
+        private bool intCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+            } 
+            else
+            {
+                MessageBox.Show("숫자만 입력해주세요", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("펑!", "알림", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtNum.ReadOnly = false;
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                this.txtCountDown.Text =
+                    Convert.ToString(this.CountOrgNum);
+            }
+        }
+
+        private void TxtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+
+            }
         }
     }
 }
